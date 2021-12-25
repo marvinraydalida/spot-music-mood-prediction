46528045b15b5a567be3d30c1dfef2b8f31eb6ed
marvinraydalida <marvinraydalida@gmail.com>
2021-12-26 04:08:41 +0800

    Added some animations in search result

--- spot/main_app/static/main_app/javascript/script.js
+++ spot/main_app/static/main_app/javascript/script.js
@@ -1,1 +1,13 @@
-const get = document.getElementsByTagName("h1")
+function cardHover(title){
+    const card = document.getElementById(title);
+    gridItemContainer = card.children[1];
+    if(gridItemContainer.children[0].innerHTML.length > 24){
+        gridItemContainer.children[0].style.animation = "slide 8s";
+    }
+}
+
+function cardLeave(title){
+    const card = document.getElementById(title);
+    gridItemContainer = card.children[1];
+    gridItemContainer.children[0].style.animation = "slideCancel";
+}
