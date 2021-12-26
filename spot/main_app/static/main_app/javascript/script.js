02357d13c9b99602e4e31f83483f8ff77fa3096b
marvinraydalida <marvinraydalida@gmail.com>
2021-12-26 23:53:48 +0800

    changes in result page

--- spot/main_app/static/main_app/javascript/script.js
+++ spot/main_app/static/main_app/javascript/script.js
@@ -1,13 +1,65 @@
 function cardHover(title){
     const card = document.getElementById(title);
     gridItemContainer = card.children[1];
-    if(gridItemContainer.children[0].innerHTML.length > 24){
-        gridItemContainer.children[0].style.animation = "slide 8s";
+    let offset = gridItemContainer.children[0].offsetWidth;
+    let scroll = gridItemContainer.children[0].scrollWidth;
+    if(scroll > offset){
+        transformLen = ((scroll - offset)/offset)*-100;
+        transitionDuration = scroll/offset;
+        gridItemContainer.children[0].style.transition = "transform " +transitionDuration + "s linear";
+        gridItemContainer.children[0].style.transform = "translateX(" + transformLen + "%)";
     }
 }
 
 function cardLeave(title){
     const card = document.getElementById(title);
     gridItemContainer = card.children[1];
-    gridItemContainer.children[0].style.animation = "slideCancel";
+    gridItemContainer.children[0].style.transition = "transform 2s ease-out";
+    gridItemContainer.children[0].style.transform = "translateX(0%)";
+    console.log("left");
+}
+
+function cardSubmit(title){
+    const card = document.getElementById(title);
+    hiddenForm = card.children[2];
+    hiddenForm.submit();
+}
+
+function controlsClick(){
+    const btn = document.getElementById("controls");
+    const preview_src = document.getElementById("audio");
+    console.log(btn.dataset.status);
+    console.log("clicked");
+    if(btn.dataset.status === "paused"){
+  	    btn.style.transition = "clip-path 200ms ease-out";
+  	    btn.style.clipPath = "polygon(0 0, 40% 0, 40% 100%, 60% 100%, 60% 0, 100% 0, 100% 100%,0 100%)";
+        preview_src.play();
+        btn.dataset.status = "played";
+    }
+    else if(btn.dataset.status === "played"){
+  	    btn.style.clipPath = "polygon(0 0, 50% 25%, 50% 75%, 50% 75%, 50% 25%, 100% 50%, 100% 50%, 0% 100%)";
+        preview_src.pause();
+        btn.dataset.status = "paused";
+    }
+}
+
+function previewHover(){
+    const card = document.getElementById("preview-grid");
+    gridItemContainer = card.children[3];
+    let offset = gridItemContainer.children[0].offsetWidth;
+    let scroll = gridItemContainer.children[0].scrollWidth;
+    if(scroll > offset){
+        transformLen = ((scroll - offset)/offset)*-100;
+        transitionDuration = scroll/offset;
+        gridItemContainer.children[0].style.transition = "transform " +transitionDuration + "s linear";
+        gridItemContainer.children[0].style.transform = "translateX(" + transformLen + "%)";
+    }
+}
+
+function previewLeave(){
+    const card = document.getElementById("preview-grid");
+    gridItemContainer = card.children[3];
+    gridItemContainer.children[0].style.transition = "transform 2s ease-out";
+    gridItemContainer.children[0].style.transform = "translateX(0%)";
+    console.log("left");
 }
