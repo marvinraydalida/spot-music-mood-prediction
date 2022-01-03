516972213b6cd789eab55a6602d55a86050f158a
marvinraydalida <marvinraydalida@gmail.com>
2022-01-04 00:19:41 +0800

    Updated scripts, dataset, and model.

---
+++ spot/modules/sklearnFunctions.py
@@ -0,0 +1,42 @@
+from typing import AsyncIterable
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+
+
+def predict_track(audio_features):
+    
+    #data = pd.read_csv('./audio_features_dataset.csv')
+
+    data = pd.read_csv('./audio_features_dataset.csv')
+
+    feature_cols = [
+        'danceability',
+        'acousticness',
+        'energy',
+        'instrumentalness',
+        'key',
+        'liveness',
+        'loudness',
+        'speechiness',
+        'tempo',
+        'time_signature',
+        'valence',
+        'mode'
+    ]
+    #X is the feature matrix
+    X = data[feature_cols]
+    #y is the vector label
+    y = data.mood_string
+
+    
+    #gradient_boost_model = GradientBoostingClassifier()
+    #gradient_boost_model.fit(X,y)
+
+    randomForestClassifier = RandomForestClassifier()
+    randomForestClassifier.fit(X,y)
+    track_af = pd.json_normalize(audio_features)
+    X_pred = track_af[feature_cols]
+
+    #return gradient_boost_model.predict(X_pred)
+
+    return randomForestClassifier.predict(X_pred)
